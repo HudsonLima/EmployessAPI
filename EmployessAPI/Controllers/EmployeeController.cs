5dcb153da7823e050b630bff7d722f86643c0bfa
Hudson Lima <hudsonlima_@hotmail.com>
2018-02-13 19:11:44 -0200

    Addind Try Catch

--- EmployessAPI/Controllers/EmployeeController.cs
+++ EmployessAPI/Controllers/EmployeeController.cs
@@ -19,132 +19,172 @@ namespace EmployessAPI.Controllers
     {
         private EmployeesManagerEntities db = new EmployeesManagerEntities();
 
-        // GET: api/Employees
+        // GET: api/Employees/?&page_size=1&page=1
         public IEnumerable<employee> Getemployees([FromUri]PagingParameterModel pagingparametermodel)
         {
-            // Return List of Customer  
-            var source = (from employees in db.employees.
-                            OrderBy(a => a.Name)
-                          select employees).AsQueryable();
 
-            // Get's No of Rows Count   
-            int count = source.Count();
-
-            // Parameter is passed from Query string if it is null then it default Value will be pageNumber:1  
-            int CurrentPage = pagingparametermodel.page;
-
-            // Parameter is passed from Query string if it is null then it default Value will be pageSize:20  
-            int PageSize = pagingparametermodel.page_size;
-
-            // Display TotalCount to Records to User  
-            int TotalCount = count;
-
-            // Calculating Totalpage by Dividing (No of Records / Pagesize)  
-            int TotalPages = (int)Math.Ceiling(count / (double)PageSize);
-
-            // Returns List of Customer after applying Paging   
-            var items = source.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
-
-            // if CurrentPage is greater than 1 means it has previousPage  
-            var previousPage = CurrentPage > 1 ? "Yes" : "No";
-
-            // if TotalPages is greater than CurrentPage means it has nextPage  
-            var nextPage = CurrentPage < TotalPages ? "Yes" : "No";
-
-            // Object which we are going to send in header   
-            var paginationMetadata = new
+            try
             {
-                totalCount = TotalCount,
-                pageSize = PageSize,
-                currentPage = CurrentPage,
-                totalPages = TotalPages,
-                previousPage,
-                nextPage
-            };
 
-            // Setting Header  
-            HttpContext.Current.Response.Headers.Add("Paging-Headers", JsonConvert.SerializeObject(paginationMetadata));
-            // Returing List of Customers Collections  
-            return items;
+                // Return List of Employees  
+                var source = (from employees in db.employees.
+                                OrderBy(a => a.Name)
+                              select employees).AsQueryable();
+
+                // Get's No of Rows Count   
+                int count = source.Count();
+
+                // Parameter is passed from Query string if it is null then it default Value will be pageNumber:1  
+                int CurrentPage = pagingparametermodel.page;
+
+                // Parameter is passed from Query string if it is null then it default Value will be pageSize:20  
+                int PageSize = pagingparametermodel.page_size;
+
+                // Display TotalCount to Records to User  
+                int TotalCount = count;
+
+                // Calculating Totalpage by Dividing (No of Records / Pagesize)  
+                int TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+
+                // Returns List of Employees after applying Paging   
+                var items = source.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
+
+                // if CurrentPage is greater than 1 means it has previousPage  
+                var previousPage = CurrentPage > 1 ? "Yes" : "No";
+
+                // if TotalPages is greater than CurrentPage means it has nextPage  
+                var nextPage = CurrentPage < TotalPages ? "Yes" : "No";
+
+                // Object which we are going to send in header   
+                var paginationMetadata = new
+                {
+                    totalCount = TotalCount,
+                    pageSize = PageSize,
+                    currentPage = CurrentPage,
+                    totalPages = TotalPages,
+                    previousPage,
+                    nextPage
+                };
+
+                // Setting Header  
+                HttpContext.Current.Response.Headers.Add("Paging-Headers", JsonConvert.SerializeObject(paginationMetadata));
+                // Returing List of Employees Collections  
+                return items;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         // GET: api/Employee/5
         [ResponseType(typeof(employee))]
         public IHttpActionResult Getemployee(int id)
         {
-            employee employee = db.employees.Find(id);
-            if (employee == null)
+            try
             {
-                return NotFound();
-            }
 
-            return Ok(employee);
+                employee employee = db.employees.Find(id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(employee);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         // PUT: api/Employee/5
         [ResponseType(typeof(void))]
         public IHttpActionResult Putemployee(int id, employee employee)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != employee.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(employee).State = EntityState.Modified;
-
             try
             {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!employeeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return StatusCode(HttpStatusCode.NoContent);
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (id != employee.Id)
+                {
+                    return BadRequest();
+                }
+
+                db.Entry(employee).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!employeeExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         // POST: api/Employee
         [ResponseType(typeof(employee))]
         public IHttpActionResult Postemployee(employee employee)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                db.employees.Add(employee);
+                db.SaveChanges();
+
+                return CreatedAtRoute("DefaultApi", new { id = employee.Id }, employee);
             }
-
-            db.employees.Add(employee);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = employee.Id }, employee);
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         // DELETE: api/Employee/5
         [ResponseType(typeof(employee))]
         public IHttpActionResult Deleteemployee(int id)
         {
-            employee employee = db.employees.Find(id);
-            if (employee == null)
+            try
             {
-                return NotFound();
+
+                employee employee = db.employees.Find(id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+
+                db.employees.Remove(employee);
+                db.SaveChanges();
+
+                return Ok(employee);
             }
-
-            db.employees.Remove(employee);
-            db.SaveChanges();
-
-            return Ok(employee);
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         protected override void Dispose(bool disposing)
